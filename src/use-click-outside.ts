dd6aa7a68465892bdbf187e1f99c3db7b2a8875e
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-07-27 09:29:02 +0430

    Add files via upload

---
+++ src/use-click-outside.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+interface ArgsType {
+  id: string;
+  isRunning: boolean;
+  fnToListen?: Function;
+  callbackFn?: Function;
+}
+
+export default function useClickOutside(args: ArgsType): void {
+  const { id, isRunning, fnToListen, callbackFn } = args;
+
+  useEffect(() => {
+    !!fnToListen && fnToListen();
+
+    if (isRunning) {
+      document.addEventListener('click', handleClick);
+      document.addEventListener('ontouchstart', handleClick);
+    }
+    return () => {
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('ontouchstart', handleClick);
+    };
+  }, [isRunning]);
+
+  const handleClick = (e) => {
+    if (!e.target.closest('#' + id)) callbackFn();
+  };
+}
