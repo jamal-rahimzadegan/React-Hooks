99bfc306b152cda4a62abc69c4a48f4f936254e3
Jamal RahimZadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-09-29 12:49:46 +0330

    Added usePosition

---
+++ src/use-position.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+type PosType = { latitude: number; longitude: number; error: unknown };
+
+export default function usePosition(): PosType {
+  const [position, setPosition] = useState<PosType>({ latitude: 0, longitude: 0, error: null });
+
+  const handleChange = (pos) => {
+    const { latitude, longitude } = pos.coords;
+
+    setPosition({
+      ...position,
+      latitude,
+      longitude,
+    });
+  };
+
+  const handleError = (error) => setPosition({ ...position, error });
+
+  useEffect(() => {
+    const geo = navigator?.geolocation;
+
+    if (!geo) {
+      handleError('Geolocation is not supported');
+      return;
+    }
+
+    const watcher = geo.watchPosition(handleChange, handleError);
+    return () => geo.clearWatch(watcher);
+  }, []);
+
+  return position;
+}
