72fe029b898bb19605dfdf6782119b7628ff073c
Jamal <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-11-23 10:54:14 +0330

    Update use-render-count.ts

--- src/use-render-count.ts
+++ src/use-render-count.ts
@@ -1,4 +1,6 @@
-export default function useRenderCount(target:string) {
+import { useRef } from 'react';
+
+export default function useRenderCount(target: string) {
   const ref = useRef(1); // minimum times of render
   console.info(`🔥 ${target} rendered ${ref.current++} times`);
 }
