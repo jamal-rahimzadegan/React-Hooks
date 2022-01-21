5ececbdaf5d2d7e37794a0f72e5e8c31721418dd
Jamal <jamalrahimzadegan@gmail.com>
2022-01-21 23:24:19 +0330

    Create use-debounce.tsx

---
+++ src/use-debounce.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+export default function useDebounce(value: string, delay: number): string {
+	const [debouncedValue, setDebouncedValue] = useState<string>(value);
+
+	useEffect(
+		() => {
+			const handler = setTimeout(() => setDebouncedValue(value), delay);
+			return () => clearTimeout(handler);
+		},
+		[value]
+	);
+
+	return debouncedValue;
+}
