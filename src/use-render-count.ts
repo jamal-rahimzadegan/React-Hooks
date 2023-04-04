dd0f037671902c4ad55eb600dcf2d42d267efb16
Jamal <jamalrahimzadegan@gmail.com>
2023-04-04 16:01:19 +0100

    Delete use-render-count.ts

--- src/use-render-count.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-
-// Counts the number of renders
-// `target` is the name of the Component
-// Minimum render is `1`
-export default function useRenderCount(target: string) {
-  let ref = useRef(1);
-  console.info(`🔥 ${target} rendered ${ref.current++} times`);
-}
